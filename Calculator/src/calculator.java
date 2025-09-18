9a8615a61d1eb8d08721e76f8ea3735ef485776d
Sergiu Jul <sergiu.julavschi@mail.ru>
2025-09-18 22:28:33 +0300

    added bar for calculator

---
+++ Calculator/src/calculator.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton,subButton,multButton,divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free",Font.BOLD,30);
+
+    double num1=0,num2=0,result=0;
+    char operator;
+
+    calculator() {
+
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(50, 25, 300, 50);
+        textfield.setFont(myFont);
+
+        frame.add(textfield);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        calculator calc= new calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
+    }
+}
